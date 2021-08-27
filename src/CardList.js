4bc80f216a2b5c85c253cddd1d466854798b7726
Ian Hawe <ianhawe24@hotmail.co.uk>
2021-08-27 15:37:05 +0100

    displaying api names of champions

--- src/CardList.js
+++ src/CardList.js
@@ -1,9 +1,20 @@
 import React from "react";
 
 const CardList = (props) => {
+
+  let champions = props.champions ? Object.entries(props.champions.data) : [];
+  console.log(champions);
+
   return (
     <div>
-        Hello world from CardList
+      {champions.length > 0 &&
+        champions.map((champion) => (
+          <div>
+            <p> {champion[0]}</p>
+   
+            
+          </div>
+        ))}
     </div>
   );
 };
