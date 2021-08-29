d9e5417bd1ec67ba4438ad81d3b19022b4fb8feb
Ian Hawe <ianhawe24@hotmail.co.uk>
2021-08-29 14:27:28 +0100

    grouped stats based on combat relevance

--- src/CardList.js
+++ src/CardList.js
@@ -6,7 +6,7 @@ const CardList = (props) => {
   let removeStat = (stat) => {
     console.log(1, stat);
     if (stat === 0) {
-      return 'N/A' ;
+      return "N/A";
     }
   };
 
@@ -21,28 +21,42 @@ const CardList = (props) => {
               alt="bob"
             />
             <ul>
-              {" "}
               Stats
+              {/* Offensive Stats*/}
+              <li>Attack Range: {champion[1].stats.attackrange}</li>
+              <li>
+                Attack Damage Scale: {champion[1].stats.attackdamageperlevel}
+              </li>
+              <li>
+                Attack Speed Scale: {champion[1].stats.attackspeedperlevel}
+              </li>
+              <li>Attack Speed: {champion[1].stats.attackspeed}</li>
+              <li>Critical Chance: {removeStat(champion[1].stats.crit)}</li>
+              <li>
+                Critical Chance per level:{" "}
+                {removeStat(champion[1].stats.critperlevel)}
+              </li>
+            </ul>
+            {/* Defensive Stats*/}
+            <ul>
               <li>Hp: {champion[1].stats.hp}</li>
               <li>Hp Scale: {champion[1].stats.hpperlevel}</li>
               <li>HP Regen: {champion[1].stats.hpregen}</li>
               <li>HP Regen Scale: {champion[1].stats.hpregenperlevel}</li>
-              <li>Mana: {removeStat(champion[1].stats.mp)}</li>
-              <li>MP Regen: {removeStat(champion[1].stats.mpregen)}</li>
-              <li>MP Regen Scale: {removeStat(champion[1].stats.mpregenperlevel)}</li>
-              <li>Mana Scale: {removeStat(champion[1].stats.mpperlevel)}</li>
               <li>Armour: {champion[1].stats.movespeed}</li>
               <li>Armour Scale: {champion[1].stats.armor}</li>
               <li>Spell Block: {champion[1].stats.spellblock}</li>
               <li>Spell Block Scale: {champion[1].stats.spellblockperlevel}</li>
-              <li>Attack Range: {champion[1].stats.attackrange}</li>
-              <li>Critical Chance: {removeStat(champion[1].stats.crit)}</li>
+            </ul>
+
+            {/* Other Stats*/}
+            <ul>
+              <li>Mana: {removeStat(champion[1].stats.mp)}</li>
+              <li>MP Regen: {removeStat(champion[1].stats.mpregen)}</li>
               <li>
-                Critical Chance per level: {removeStat(champion[1].stats.critperlevel)}
+                MP Regen Scale: {removeStat(champion[1].stats.mpregenperlevel)}
               </li>
-              <li>Attack Damage Scale: {champion[1].stats.attackdamageperlevel}</li>
-              <li> Attack Speed Scale: {champion[1].stats.attackspeedperlevel}</li>
-              <li>Attack Speed: {champion[1].stats.attackspeed}</li>
+              <li>Mana Scale: {removeStat(champion[1].stats.mpperlevel)}</li>
             </ul>
           </div>
         ))}
