cd85a64f52840018d402e0ae69f49c01749803b2
Ian Hawe <ianhawe24@hotmail.co.uk>
2021-08-29 14:22:47 +0100

    created a funcion to remove stats that represent 0 to 'N/A'

--- src/CardList.js
+++ src/CardList.js
@@ -3,6 +3,13 @@ import React from "react";
 const CardList = (props) => {
   let champions = props.champions ? Object.entries(props.champions.data) : [];
 
+  let removeStat = (stat) => {
+    console.log(1, stat);
+    if (stat === 0) {
+      return 'N/A' ;
+    }
+  };
+
   return (
     <div>
       {champions.length > 0 &&
@@ -18,27 +25,23 @@ const CardList = (props) => {
               Stats
               <li>Hp: {champion[1].stats.hp}</li>
               <li>Hp Scale: {champion[1].stats.hpperlevel}</li>
-              <li>Mana: {champion[1].stats.mp}</li>
-              <li>Mana Scale: {champion[1].stats.mpperlevel}</li>
+              <li>HP Regen: {champion[1].stats.hpregen}</li>
+              <li>HP Regen Scale: {champion[1].stats.hpregenperlevel}</li>
+              <li>Mana: {removeStat(champion[1].stats.mp)}</li>
+              <li>MP Regen: {removeStat(champion[1].stats.mpregen)}</li>
+              <li>MP Regen Scale: {removeStat(champion[1].stats.mpregenperlevel)}</li>
+              <li>Mana Scale: {removeStat(champion[1].stats.mpperlevel)}</li>
               <li>Armour: {champion[1].stats.movespeed}</li>
               <li>Armour Scale: {champion[1].stats.armor}</li>
               <li>Spell Block: {champion[1].stats.spellblock}</li>
               <li>Spell Block Scale: {champion[1].stats.spellblockperlevel}</li>
               <li>Attack Range: {champion[1].stats.attackrange}</li>
-              <li>HP Regen: {champion[1].stats.hpregen}</li>
-              <li>HP Regen Scale: {champion[1].stats.hpregenperlevel}</li>
-              <li>MP Regen: {champion[1].stats.mpregen}</li>
-              <li>MP Regen Scale: {champion[1].stats.mpregenperlevel}</li>
-              <li>Critical Chance: {champion[1].stats.crit}</li>
+              <li>Critical Chance: {removeStat(champion[1].stats.crit)}</li>
               <li>
-                Critical Chance per level: {champion[1].stats.critperlevel}
+                Critical Chance per level: {removeStat(champion[1].stats.critperlevel)}
               </li>
-              <li>
-                Attack Damage Scale: {champion[1].stats.attackdamageperlevel}
-              </li>
-              <li>
-                Attack Speed Scale: {champion[1].stats.attackspeedperlevel}
-              </li>
+              <li>Attack Damage Scale: {champion[1].stats.attackdamageperlevel}</li>
+              <li> Attack Speed Scale: {champion[1].stats.attackspeedperlevel}</li>
               <li>Attack Speed: {champion[1].stats.attackspeed}</li>
             </ul>
           </div>
