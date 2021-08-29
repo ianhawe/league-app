8458c51de6dbf30fa39da85ffd91d77813ecc3b5
Ian Hawe <ianhawe24@hotmail.co.uk>
2021-08-29 17:53:40 +0100

    added cardlist items to props for Card

--- src/CardList.js
+++ src/CardList.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Card from "./Card";
 
 const CardList = (props) => {
   let champions = props.champions ? Object.entries(props.champions.data) : [];
@@ -18,60 +19,62 @@ const CardList = (props) => {
     <div>
       {champions.length > 0 &&
         champions.map((champion) => (
-          <div>
-            <h3> {champion[0]}</h3>
-            <img
-              src={`http://ddragon.leagueoflegends.com/cdn/11.16.1/img/champion/${champion[1].image.full}`}
-              alt="bob"
-            />
+          <Card
+            name={champion[0]}
+            pic={`http://ddragon.leagueoflegends.com/cdn/11.16.1/img/champion/${champion[1].image.full}`}
+            /* Offensive Stats*/
+            atkrng={removeStat("Attack Range: ", champion[1].stats.attackrange)}
+            atkdmg={removeStat(
+              "Attack Damage: ",
+              champion[1].stats.attackdamage
+            )}
+            atkspd={removeStat(
+              "Attack Speed: ",
+              champion[1].stats.attackspeed
+            )}
+            atkdmgscl={removeStat(
+              "Attack Damage Scale: ",
+              champion[1].stats.attackdamageperlevel
+            )}
+            atkspdscl={removeStat(
+              "Attack Speed Scale: ",
+              champion[1].stats.attackspeedperlevel
+            )}
+            cricha={removeStat("Critical Chance: ", champion[1].stats.crit)}
+            crichascl={removeStat(
+              "Critical Chance per level: ",
+              champion[1].stats.critperlevel
+            )}
+            /*  Defensive Stats*/
 
-            <h2>Offensive Stats</h2>
-            <ul>
-              {/* Offensive Stats*/}
-              {removeStat("Attack Range: ", champion[1].stats.attackrange)}
-              {removeStat("Attack Damage: ", champion[1].stats.attackdamage)}
-              {removeStat("Attack Speed: ", champion[1].stats.attackspeed)}
-              {removeStat(
-                "Attack Damage Scale: ",
-                champion[1].stats.attackdamageperlevel
-              )}
-              {removeStat(
-                "Attack Speed Scale: ",
-                champion[1].stats.attackspeedperlevel
-              )}
-              {removeStat("Critical Chance: ", champion[1].stats.crit)}
-              {removeStat(
-                "Critical Chance per level: ",
-                champion[1].stats.critperlevel
-              )}
-            </ul>
+            hp={removeStat("Hp: ", champion[1].stats.hp)}
+            hpscl={removeStat("Hp Scale: ", champion[1].stats.hpperlevel)}
+            hprgn={removeStat("HP Regen: ", champion[1].stats.hpregen)}
+            hprgnscl={removeStat(
+              "HP Regen Scale: ",
+              champion[1].stats.hpregenperlevel
+            )}
+            arm={removeStat("Armour: ", champion[1].stats.movespeed)}
+            armscl={removeStat("Armour Scale: ", champion[1].stats.armor)}
+            splblk={removeStat("Spell Block: ", champion[1].stats.spellblock)}
+            splblkscl={removeStat(
+              "Spell Block Scale: ",
+              champion[1].stats.spellblockperlevel
+            )}
+            /* Other Stats*/
 
-            {/* Defensive Stats*/}
-            <h2>Defensive </h2>
-            <ul>
-              {removeStat("Hp Scale: ", champion[1].stats.hpperlevel)}
-              {removeStat("Hp: ", champion[1].stats.hp)}
-              {removeStat("HP Regen: ", champion[1].stats.hpregen)}
-              {removeStat(
-                "HP Regen Scale: ",
-                champion[1].stats.hpregenperlevel
-              )}
-              {removeStat("Armour: ", champion[1].stats.movespeed)}
-              {removeStat("Armour Scale: ", champion[1].stats.armor)}
-              {removeStat("Spell Block: ", champion[1].stats.spellblock)}
-              {removeStat(
-                "Spell Block Scale: ",
-                champion[1].stats.spellblockperlevel
-              )}
-            </ul>
-            {/* Other Stats*/}
-            <ul>
-             { removeStat( "Resource: ", champion[1].stats.mp  )}
-             { removeStat( "Resource Regen: ", champion[1].stats.mpregen )}
-             { removeStat( "Resource Scale: ", champion[1].stats.mpperlevel )}
-             { removeStat( "Resource Regen Scale: ", champion[1].stats.mpregenperlevel )}
-            </ul>
-          </div>
+            rsc={removeStat("Resource: ", champion[1].stats.mp)}
+            rscrgn={removeStat("Resource Regen: ", champion[1].stats.mpregen)}
+            rscscl={removeStat(
+              "Resource Scale: ",
+              champion[1].stats.mpperlevel
+            )}
+            rscrgnscl={removeStat(
+              "Resource Regen Scale: ",
+              champion[1].stats.mpregenperlevel
+            )}
+          />
+          
         ))}
     </div>
   );
